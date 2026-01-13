5ce3138dc126e4e692bc80ce62d5a2dd5ad6cb59
Tomas Roun <tomas.roun8@gmail.com>
2026-01-13 21:19:36 +0100

    Update default layout

--- keyboards/ida/keymaps/default/keymap.c
+++ keyboards/ida/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A,    KC_B,
-        KC_C
+        KC_VOLD,    LGUI(KC_L),
+        KC_VOLU
     )
 };
